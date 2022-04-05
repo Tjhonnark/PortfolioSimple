60b5499d5280322591842fedf9bcf2ab77ca5dc3
Thomaz <tjhonnar@gmail.com>
2022-04-05 15:39:59 -0500

    PS v2.1

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -8,6 +8,7 @@ export default function Layout(props){
         <Head>
             <title>Portfolio</title>
             <meta property="og:title" content="Portfolio" />
+            <meta property="og:description" content="Desarrollador de sitios web y landing page. Con capacidades de resolución de conflictos y de aprender nuevas tecnologías para estar siempre a la vanguardia." />
             <meta property="og:type" content="website" />
             <meta property="og:url" content="https://tjhonnar.ga" />
             <meta property="og:image" content="https://tjhonnar.ga/image0.jpg" />
