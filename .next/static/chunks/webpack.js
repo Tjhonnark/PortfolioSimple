1c57b591671a1c7846a93a98afcc8cb68c04b84c
Thomaz <tjhonnar@gmail.com>
2022-04-05 00:19:00 -0500

    PS v1.4

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "53d535853fe9b773"; }
+/******/ 		__webpack_require__.h = function() { return "3f86a70c7cf472d1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default function Index(){
                                 <img className="img-fluid" src="image0.jpg" alt=""/>
                             </div>
                             <div className="col-md-8">
-                                <h1>Rodrigo García test</h1>
+                                <h1>Rodrigo García</h1>
                                 <h3>Developer Junior</h3>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem minima ad magnam unde. Reprehenderit error possimus placeat, atque voluptatem repudiandae nostrum dolorem vero eligendi rem? Ullam dolor praesentium illum beatae.</p>
                                 <a href="/hireme">Hire Me</a>
