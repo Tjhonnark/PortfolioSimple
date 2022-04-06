e20313d66aef16256716b03cec070f9518e70903
Thomaz <tjhonnar@gmail.com>
2022-04-05 21:36:53 -0500

    PS v3.4

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,12 +3,29 @@ import Navbar from './Navbar';
 export default function Layout(props){
     return (
         <>
-        <Navbar/>
-        <main className='container py-4'>
-            {props.children}
-        </main> 
-        
-        
+            <Head>
+                <title>My Portfolio</title>
+                <meta name="description" content="Desarrollador de sitios web y landing page. Con capacidades de resolución de conflictos y de aprender nuevas tecnologías para estar siempre a la vanguardia." />
+
+                {/* FACEBOOK */}
+                <meta property="og:title" content="MyPortfolio" />
+                <meta property="og:description" content="Desarrollador de sitios web y landing page. Con capacidades de resolución de conflictos y de aprender nuevas tecnologías para estar siempre a la vanguardia." />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://tjhonnar.ga" />
+                <meta property="og:image" content="https://tjhonnar.ga/image0.jpg" />
+                {/* TWITTER */}
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta name="twitter:title" content="MyPortfolio"/>
+                <meta name="twitter:creator" content="@Roderix78"/>
+
+                {/* APPLE */}
+
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <Navbar/>
+            <main className='container py-4'>
+                {props.children}
+            </main> 
         </>
     )
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,25 +11,6 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
-                    <title>My Portfolio</title>
-                    <meta name="description" content="Desarrollador de sitios web y landing page. Con capacidades de resolución de conflictos y de aprender nuevas tecnologías para estar siempre a la vanguardia." />
-
-                    {/* FACEBOOK */}
-                    <meta property="og:title" content="MyPortfolio" />
-                    <meta property="og:description" content="Desarrollador de sitios web y landing page. Con capacidades de resolución de conflictos y de aprender nuevas tecnologías para estar siempre a la vanguardia." />
-                    <meta property="og:type" content="website" />
-                    <meta property="og:url" content="https://tjhonnar.ga" />
-                    <meta property="og:image" content="https://tjhonnar.ga/image0.jpg" />
-
-                    {/* TWITTER */}
-                    <meta name="twitter:card" content="summary_large_image"/>
-                    <meta name="twitter:title" content="MyPortfolio"/>
-                    <meta name="twitter:creator" content="@Roderix78"/>
-
-                    {/* APPLE */}
-
-                    <link rel="icon" href="/favicon.ico"/>
-
                     <script
                         dangerouslySetInnerHTML={{
                             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -39,7 +20,6 @@ class MyDocument extends Document {
                             })(window,document,'script','dataLayer','${GTM_ID}');`,
                         }}
                     />
-                    
                 </Head>
                 <body>
                 <noscript
